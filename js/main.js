2685a6e2b51ca31663a1103d082f0554ec9b9365
Your Name <pablorincones95@gmail.com>
2018-11-14 10:23:30 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+(function(window) {
+  // Transition effect for navbar 
+  $(window).scroll(function() {
+    // checks if window is scrolled more than 500px, adds/removes solid class
+    if ($(this).scrollTop() > 500) {
+      $('.navbar').addClass('solid');
+    } else {
+      $('.navbar').removeClass('solid');
+    }
+  });
+})(window);
