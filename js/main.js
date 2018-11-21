417ca9b6c5b0c7c42cef76c5b59737b8c167d486
Your Name <pablorincones95@gmail.com>
2018-11-21 10:27:42 -0400

    detalles de diseño

--- js/main.js
+++ js/main.js
@@ -1,28 +1,4 @@
-(function(window) {
-  console.log('gawfwa');
-  // Transition effect for navbar 
-  $(window).scroll(function() {
-    // checks if window is scrolled more than 500px, adds/removes solid class
-    if ($(this).scrollTop() > 500) {
-      $('.navbar').addClass('solid');
-    } else {
-      $('.navbar').removeClass('solid');
-    }
-  });
-})(window);
-
-
-$('#formulario').submit(function() {
-  var names = document.getElementById('names').value;
-  var url = 'vendor/phpmailer/enviar.php';
-  console.log(names);
-  $.ajax({
-    type: 'POST',
-    url: url,
-    data: $('#formulario').serialize(),
-    success: function(data) {
-      console.log('data')
-    }
-  });
-  return false;
+'use strict';
+document.querySelector('.navbar-toggler').addEventListener('click', () => {
+  document.querySelector('#navbarSupportedContent').classList.toggle('show')
 });
